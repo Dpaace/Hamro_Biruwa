c1b037a2fb198886d1ac05973d0068b86a1ca463
Dpaace <dipeshnepali767@gmail.com>
2022-01-17 23:30:29 +0545

    Add product in backend and show in shop and featured

--- Biruwa/Hamro/views.py
+++ Biruwa/Hamro/views.py
@@ -5,9 +5,15 @@ from django.shortcuts import redirect, render, HttpResponse
 from django.urls.conf import include
 from django.contrib import messages
 
+from product.models import Product
+
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html')
+    featured_product = Product.objects.order_by('-created_date').filter(is_featured=True)
+    data = {
+        'featured_product': featured_product,
+    }
+    return render(request, 'pages/home.html', data)
 
 def about(request):
     return render(request, 'pages/about.html')
